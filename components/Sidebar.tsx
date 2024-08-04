606d132ce9ebbb1206e00e4349d3b3a16c24d5a1
Aviral Singh <aviralsingh3501@gmail.com>
2024-08-04 10:40:56 +0530

    Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,37 +1,56 @@
 "use client";
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 import NewDocumentButton from "./NewDocumentButton";
-import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const menuOptions = (
+    <>
+   <NewDocumentButton/>
+   {/* My documents */}
+
+   {/* My Lists */}
+
+   {/* Shared with me */}
+
+   {/* List */}
+    </>
+    
+);
 
 const Sidebar = () => {
   return (
     <div className="bg-red-200 p-2 md:p-5 relative">
-      <NewDocumentButton />
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon
+            className="p-2 hover:opacity-30 rounded-lg" size={40}/>
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+             
+              <div className="">
+                {/* Options */}
+                {menuOptions}
+              </div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+
+      <div className="hidden md:inline">
+        {menuOptions}
+      </div>
     </div>
   );
 };
