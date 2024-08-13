dd06d0409fa5d56b603488b5df334020371d3985
Aviral Singh <aviralsingh3501@gmail.com>
2024-08-13 14:11:34 +0530

    display name added

--- components/Avatars.tsx
+++ components/Avatars.tsx
@@ -48,6 +48,8 @@ const MemoizedAvatar = React.memo(
     prevProps.self?.id === nextProps.self?.id
 );
 
+MemoizedAvatar.displayName = "MemoizedAvatar";
+
 const Avatars = () => {
   const others = useOthers();
   const self = useSelf();
@@ -68,46 +70,3 @@ const Avatars = () => {
 export default Avatars;
 
 
-// "use client";
-
-// import { useOthers, useSelf } from "@liveblocks/react/suspense";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-// import {
-//   Tooltip,
-//   TooltipContent,
-//   TooltipProvider,
-//   TooltipTrigger,
-// } from "@/components/ui/tooltip";
-
-// const Avatars = () => {
-//   const others = useOthers();
-//   const self = useSelf();
-//   const all = [self, ...others];
-
-//   return (
-//     <div className="flex gap-2 items-center justify-center">
-//       <p className="font-light text-sm">Users currently editing this page</p>
-//       <div className=" flex -space-x-5">
-//         {all.map((other, i) => (
-//           <TooltipProvider key={other?.id + i + Math.random() * 99}>
-//             <Tooltip>
-//               <TooltipTrigger>
-//                 {" "}
-//                 <Avatar className="border-2 hover:z-50">
-//                   <AvatarImage
-//                     src={other?.info.avatar || "https://github.com/shadcn.png"}
-//                   />
-//                   <AvatarFallback>CN</AvatarFallback>
-//                 </Avatar>
-//               </TooltipTrigger>
-//               <TooltipContent>
-//                 <p>{self?.id === other?.id ? "You" : other?.info.name}</p>
-//               </TooltipContent>
-//             </Tooltip>
-//           </TooltipProvider>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-// export default Avatars;
